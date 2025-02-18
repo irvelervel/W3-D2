ae78b7e8eba890ccf44ca2a32570f4b31a67cbad
Stefano Casasola <ste.casasola@gmail.com>
2025-02-18 09:56:41 +0100

    First 2 methods of traversing

---
+++ script.js
@@ -0,0 +1,40 @@
+// ATTRAVERSAMENTO E MANIPOLAZIONE DEL DOM
+// Tutte le web app moderne prevedono un'interazione con il documento HTML e CSS
+// in una fase SUCCESSIVA al caricamento iniziale!
+
+// È fondamentale sapere come interagire con la pagina tramite JS perchè in tantissime
+// situazioni un contenuto "statico" non è adatto alla nostra esperienza web
+// es. Un'app di un calendario non può sperare di arrivare con sempre la data corretta
+// nell'HTML!
+
+// Questo processo di interazione avviene in DUE FASI:
+// 1) ATTRAVERSAMENTO del DOM -> c'è bisogno di imparare come raggiungere gli elementi
+// che intendiamo modificare
+// 2) MANIPOLAZIONE del DOM -> serie di tecniche che ci permettono di ALTERARE un elemento
+// es. cambiarne il testo, le proprietà CSS, le classi CSS, eliminare/aggiungere figli etc.
+
+// 1) SELEZIONE DEGLI ELEMENTI (DOM TRAVERSING)
+// tutte queste tecniche ci permetterano di "selezionare" un elemento della pagina
+// "selezionare" -> ottenere un RIFERIMENTO a quell'elemento
+// questo riferimento all'elemento sarà SEMPRE un OGGETTO JS
+console.log(document)
+
+// a) tramite l'attributo "id"
+const h1Title = document.getElementById('title')
+// ho recuperato un riferimento univoco all'elemento della pagina con id="title"
+console.log('h1Title', h1Title)
+const header = document.getElementById('header')
+console.log('header', header)
+
+// b) tramite l'attributo "class"
+const elements = document.getElementsByClassName('menu-element')
+// recupera i riferimenti di TUTTI gli elementi dotati di class "menu-element" e
+// le li ritorna in un "array" (una lista di elementi)
+console.log('menu elements', elements)
+// getElementsByClassName torna SEMPRE UN ARRAY!
+
+const navigations = document.getElementsByClassName('navigation')
+// ho selezionato TUTTI gli elementi con class="navigation"
+// ma ho solo UN elemento dotato di quella classe...
+console.log('navigations', navigations)
+// ...quindi in questo caso mi tornerà un ARRAY con dentro un elemento!!!
